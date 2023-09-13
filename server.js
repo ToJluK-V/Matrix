66c04f1bce134b5bcccda39c79af0abf88c895dd
ToJluK-V <57329726+ToJluK-V@users.noreply.github.com>
2023-09-13 23:34:18 +0300

    a

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const WebSocket = require('ws');
 const fs = require('fs');
 
-const MATRIX_FILE_PATH = 'https://github.com/ToJluK-V/Matrix/blob/main/matrix.txt';
+const MATRIX_FILE_PATH = '/public/matrix.txt';
 
 // Создаем WebSocket-сервер на порту 8080
 const wss = new WebSocket.Server({ port: 8080 });
